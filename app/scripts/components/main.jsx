50903258f9d174493910bc49f9b7784baaa6d4a7
dylan-gregory <jdylangregory@gmail.com>
2017-05-01 13:16:56 -0400

    Initial project scaffolding and adding dependencies

---
+++ app/scripts/components/main.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var $ = window.$ = window.jQuery = require('jquery');
+
+require('materialize-sass-origin/js/bin/materialize.js');
+require('materialize-sass-origin/js/parallax.js');
+
+class MainContainer extends React.Component{
+  render(){
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo">Logo</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+module.exports = {
+  MainContainer
+};
